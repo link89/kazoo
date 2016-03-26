a5365c76dcd2e44a8af6a341e883cf6a889a8012
Weihong,Xu <xuweihong.cn@gmail.com>
2016-03-27 05:36:42 +0800

    bootstrap cdr_mysql from skel

---
+++ applications/cdr_mysql/src/skel.hrl
@@ -0,0 +1,10 @@
+-ifndef(SKEL_HRL).
+-include_lib("whistle/include/wh_types.hrl").
+-include_lib("whistle/include/wh_log.hrl").
+-include_lib("whistle/include/wh_databases.hrl").
+
+-define(APP_NAME, <<"skel">>).
+-define(APP_VERSION, <<"4.0.0">> ).
+
+-define(SKEL_HRL, 'true').
+-endif.
